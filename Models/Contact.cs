9fb44610b8bea131f2f0f6f597a1911407b76642
ArtyomSivtsov <artemsivtsov0211@gmail.com>
2026-05-27 23:56:53 +0300

    Добавьте файлы проекта.

---
+++ Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsList.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? MobilePhone { get; set; }
+
+        public string? JobTitle { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
